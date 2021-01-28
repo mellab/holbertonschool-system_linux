f9cc6ec6d928fde8e190d62bd4306eac54682fca
Alejandro Medina <935@holbertonschool.com>
2021-01-28 05:39:07 +0000

    Add C source code files

---
+++ 0x04-readelf/100_main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Program that displays the information contained in the ELF file
+ * header of an ELF file
+ * @argc: How many arguments from OS command line
+ * @argv: The arguments from OS command line
+ * @env: The environment from shell
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *av[] = {"/usr/bin/readelf", "-W", "-s", "", NULL};
+
+	(void)argc;
+	av[3] = argv[1];
+	if (execve("/usr/bin/readelf", av, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
