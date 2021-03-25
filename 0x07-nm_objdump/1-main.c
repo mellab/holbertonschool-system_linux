f92a794c686c6d9872d1c35b2013a1df4af8077f
Alejandro Medina <935@holbertonschool.com>
2021-03-25 01:05:36 +0000

    Add GNU objdump command replica

---
+++ 0x07-nm_objdump/1-main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Reproduces the GNU objdump command
+ * @argc: Argument count passed from OS command line
+ * @argv: Arguments passed from OS command line
+ * @env: Environment from shell
+ *
+ * Return: EXIT_SUCCESS if successful, otherwise return EXIT_FAILURE
+ */
+
+int main(int argc, char **argv, char **env)
+{
+	char *av[] = = {"./hobjdump", "-sf", "", NULL};
+
+	(void)argc;
+	av[2] = argv[1];
+	if (execve("/usr/bin/objdump", av, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCESS);
+}
