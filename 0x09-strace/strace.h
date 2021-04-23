2e573da6d6e9cd411e6453c289f54dc801c4de0b
Alejandro Medina <935@holbertonschool.com>
2021-04-23 04:18:32 +0000

    Insert Commit Message Here

---
+++ 0x09-strace/strace.h
@@ -0,0 +1,17 @@
+#ifndef _STRACE_H_
+#define _STRACE_H_
+
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ptrace.h>
+#include <sys/reg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/user.h>
+#include "syscalls.h"
+
+#endif /* _STRACE_H_ */
